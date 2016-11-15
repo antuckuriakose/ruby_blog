4f0c0f59be9ed8831d00c3234649a7bf67cd683d
Midhun George <georgelive787@outlook.com>
2016-11-15 10:42:40 +0530

    updated signupform

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161111111515) do
+ActiveRecord::Schema.define(version: 20161114124445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,10 @@ ActiveRecord::Schema.define(version: 20161111111515) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "is_admin"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "date_of_birth"
+    t.string   "Gender"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
