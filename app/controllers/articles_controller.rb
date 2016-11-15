503ed93919fc13b32dc44de006611dac5f56b1ff
Antu Kuriakose <antuckuriakose@gmail.com>
2016-11-15 12:09:59 +0530

    User model updates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,10 @@ class ArticlesController < ApplicationController
 
   # before_action :authenticate_user!
   def index
-    @articles = Article.paginate(:page => params[:page], :per_page => 5).order('created_at DESC')
+    #.includes(:user) helps to resolve N+1 query problem-> Helps reduce the number of queries send to database
+    # check https://www.sitepoint.com/silver-bullet-n1-problem/ for details
+    @articles = Article.paginate(:page => params[:page], :per_page => 5).includes(:user).order('created_at DESC')
+    #@articles = Article.all().includes(:user)
   end
 
   def show
