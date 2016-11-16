aff21d0410e030ac5603bd2ac17ccb8dd48810fe
Ananthakrishnan <kannan4mi3@gmail.com>
2016-11-16 16:16:06 +0530

    Text editor added

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'cancancan', '~> 1.10'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'wysiwyg-rails'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
- Rails.application.routes.draw do
+Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "users/registrations"}
   resources :articles do
-   resources :comments
-
+    resources :comments
   end
   root 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
